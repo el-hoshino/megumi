d34b3e1cb7b1c243365f5135fb0041170b77ae05
史 翔新 <x_shi@yumemi.co.jp>
2021-09-29 17:25:01 +0900

    Fix package configurations

--- Package.swift
+++ Package.swift
@@ -5,9 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "megumi",
+    platforms: [
+        .macOS(.v10_10),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
+        .executable(
             name: "megumi",
             targets: ["megumi"]),
     ],
