386c1008c2a240f7c5f055534e4243009407ef36
史 翔新 <x_shi@yumemi.co.jp>
2021-09-29 17:25:14 +0900

    Temporarily disable test

--- Tests/megumiTests/megumiTests.swift
+++ Tests/megumiTests/megumiTests.swift
@@ -6,6 +6,6 @@ final class megumiTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(megumi().text, "Hello, World!")
+//        XCTAssertEqual(megumi().text, "Hello, World!")
     }
 }
